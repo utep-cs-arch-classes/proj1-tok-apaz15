df94721995b31e85f5e002c2b23b4fae0d003e90
apaz15 <apaz7095@gmail.com>
2021-02-13 01:31:16 -0700

    Added the implementation of space_char and non_space_char functions

--- src/lab1project.c
+++ src/lab1project.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
+#include "tokenizer.h"
 #define LIMIT 100
 
 int main(){
   char charArray[LIMIT];
-  
+
   int i = 0;
   
-  for(char inputChar; (inputChar = getchar()) != '\n' && i < LIMIT; i++){
+  for(char inputChar; (inputChar = getchar()) != '\n' && LIMIT > i; i++){
     charArray[i] = inputChar;
     putchar(inputChar);
-  }
-  printf("\n");
-  
+ }
+  printf("\n");  
+}
+
+int space_char(char c){
+  if(c == ' ' || c == '\n' || c == '\t')
+    return 1;
+}
+int non_space_char(char c){
+  if(c != ' ' || c != '\n' || c != '\t')
+    return 1;
 }
