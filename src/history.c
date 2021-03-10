8aafb4eb8d23a75232f0c635b85e296cac1644cd
apaz15 <apaz7095@gmail.com>
2021-03-09 19:42:04 -0700

    Added implementation of two history functions.

--- src/history.c
+++ src/history.c
@@ -3,15 +3,37 @@
 #include "history.h"
 
 List *init_history(){
-  
+  List *head = (List*)malloc(sizeof(List));
+  head -> root = (Item*)malloc(sizeof(Item));
+  head -> root -> id = 0;
+  head -> root -> next = NULL;
+  return head;
 }
 
 void add_history(List *list, char *str){
-  
+  List *ptr;
+  ptr = list;
+  int i = 0;
+  Item *temp = list -> root;
+  temp -> str = str;
+  temp -> id = i;
+  temp -> next = NULL;
+
+  while(ptr -> root -> next != NULL){
+    ptr  = (List*)ptr -> root -> next;
+  }
+  ptr -> root -> next = temp;
+  i++;
 }
 
 char *get_history(List *list, int id){
-  
+  Item *temp = list -> root;
+  while(temp -> next != NULL){
+    if(temp -> id == id)
+      return temp -> str;
+    else
+      temp -> next;
+  }
 }
 
 void print_history(List *list){
--- src/lab1project.c
+++ src/lab1project.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "tokenizer.h"
+#include "history.h"
 #define LIMIT 100
 char c;
 
@@ -8,6 +9,7 @@ int main(){
 
   char input[LIMIT];
   char **doublePointer;
+  List *history = init_history();
 
   int i = 0;
 
@@ -18,9 +20,13 @@ int main(){
   input[i + 1] = '\0';
   printf("\n");
 
+  add_history(history, input);
+  add_history(history, "Jackie");
+  char *c = get_history(history, 0);
+  printf("%s\n", c);
+
   doublePointer = tokenize(input);
   printf("Tokenizing\n");
   print_tokens(doublePointer);
-
 }
 
